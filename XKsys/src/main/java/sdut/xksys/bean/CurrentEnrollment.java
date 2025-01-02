29de626db1bf44950894d806788ca6f4c7ddb824
Xuyuquan-1 <2011696941@qq.com>
2025-01-02 08:03:31 +0800

    1.2 修复课程详细信息显示

---
+++ XKsys/src/main/java/sdut/xksys/bean/CurrentEnrollment.java
@@ -0,0 +1,24 @@
+package sdut.xksys.bean;
+
+
+
+public class CurrentEnrollment {
+    private int courseid;
+    private int currentenrollment;
+
+    public int getCourseid() {
+        return courseid;
+    }
+
+    public int getCurrentenrollment() {
+        return currentenrollment;
+    }
+
+    public void setCourseid(int courseid) {
+        this.courseid = courseid;
+    }
+
+    public void setCurrentenrollment(int currentenrollment) {
+        this.currentenrollment = currentenrollment;
+    }
+}
